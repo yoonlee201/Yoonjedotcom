95952e3444decc200c714c574dcc4b8d2ba8fd5c
yoonlee201 <93956761+yoonlee201@users.noreply.github.com>
2024-06-27 06:06:18 -0400

    3d namegit add .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,17 @@ const Page = () => {
             <Name />
             <div className="pointer-events-none absolute inset-0 z-10 flex h-full w-full justify-center overflow-y-auto pb-0 pt-root-top-md lg:pb-[3.5rem] lg:pt-root-top">
                 <div className="m-4 min-h-[47rem] min-w-[90vw] rounded-xl border-lg border-blue lg:max-h-[calc(100vh-18rem)] lg:min-h-[43rem] lg:min-w-[65vw]">
-                    <header className="text-krona-one text-h1 text-blue">
-                        <h1 className="flex text-end">
-                            Hi There!
-                            <br />
-                            I'm
-                        </h1>
-                        <h1>Software Engineer</h1>
+                    <header className="h-full w-full">
+                        <div className="flex pl-[2rem] pt-[4rem] text-end">
+                            <Title className="flex text-end">
+                                Hi There!
+                                <br />
+                                I'm
+                            </Title>
+                        </div>
+                        <div className="flex justify-end pr-[2rem] pt-[17rem] text-end">
+                            <Title>Software Engineer</Title>
+                        </div>
                     </header>
                 </div>
             </div>
