779ca98cd21689199da7bb14c0fcb0cf866792d7
yoonlee201 <93956761+yoonlee201@users.noreply.github.com>
2024-07-17 23:18:33 -0400

    nn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import Name from '@/app/_components/homePage/Name';
 import { Title } from './_components/Title';
 
-
 export default function Page() {
     return (
         <main className="relative h-screen overflow-hidden">
-            {/* <div className="absolute inset-0">
+            <div className="absolute inset-0">
                 <Name />
-            </div> */}
+            </div>
             {/* min-h-[3.75rem] w-full items-center justify-between gap-9 bg-blue text-beige md:min-h-[4.3rem] md:justify-start lg:min-h-[4.7rem] */}
-            <div className={`${false ? "pointer-events-none" : ""} relative z-10 flex min-h-screen w-full justify-center overflow-y-auto pb-0 pt-root-top-md lg:pb-[3.5rem] lg:pt-root-top`}>
+            <div
+                className={`${true ? 'pointer-events-none' : ''} relative z-10 flex min-h-screen w-full justify-center overflow-y-auto pb-0 pt-root-top-md lg:pb-[3.5rem] lg:pt-root-top`}>
                 <div className="m-4 min-h-[47rem] min-w-[90vw] rounded-xl border-lg border-blue lg:min-h-[43rem] lg:min-w-[65vw]">
                     <header className="h-full w-full">
                         <div className="flex pl-[2rem] pt-[4rem] text-end">
@@ -27,4 +27,4 @@ export default function Page() {
             </div>
         </main>
     );
-};
+}
