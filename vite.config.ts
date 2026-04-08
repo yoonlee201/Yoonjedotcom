a9e4765cf515382044d31c30fbc616210958ad88
yoonlee201 <93956761+yoonlee201@users.noreply.github.com>
2026-04-07 21:57:29 -0400

    fix base for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Yoonjedotcom/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
