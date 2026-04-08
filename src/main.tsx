b364da1ff8530a7d5b58dd402f88ae83f150cc48
yoonlee201 <93956761+yoonlee201@users.noreply.github.com>
2026-04-07 22:01:41 -0400

    system setting (darkmode/ lightmode)

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Layout from './components/Layout.tsx'
-
-document.documentElement.setAttribute('data-theme', 'dark')
+const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+const updateTheme = (e: MediaQueryListEvent | MediaQueryList) => {
+  document.documentElement.setAttribute('data-theme', e.matches ? 'dark' : 'light')
+}
+updateTheme(mediaQuery)
+mediaQuery.addEventListener('change', updateTheme)
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Layout />
